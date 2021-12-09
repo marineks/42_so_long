04abf3cba5aa4b199293660809c07e32b7578f64
Marine SANJUAN <marine@sanjuan.eu>
2021-12-09 18:27:23 +0100

    corrected check .ber w correct len to find .ber

--- srcs/01_Errors/check_ber.c
+++ srcs/01_Errors/check_ber.c
@@ -6,13 +6,12 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:22:18 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/12/09 16:09:22 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/12/09 18:26:05 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 #include "../../includes/get_next_line.h"
-#include <stdio.h>
 
 int	check_extension(char *path)
 {
@@ -22,8 +21,8 @@ int	check_extension(char *path)
 	len = ft_strlen(path);
 	fd = open(path, O_RDONLY);
 	close(fd);
-	if ((path[len - 2] != 'r' && path[len - 3] != 'e' && path[len - 4] != 'b'
-		&& path[len - 5] != '.') || fd < 0)
+	if ((path[len - 3] != 'b' && path[len - 2] != 'e' && path[len - 1] != 'r'
+		&& path[len - 4] != '.') || fd < 0)
 		return (FAILURE);
 	else
 		return (SUCCESS);
