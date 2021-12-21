0275f714fc9552372530278355c83cafd8b49e5c
Marine SANJUAN <marine@sanjuan.eu>
2021-12-21 14:11:47 +0100

    42 norm ok

--- bonus/01_Errors/check_ber_bonus.c
+++ bonus/01_Errors/check_ber_bonus.c
@@ -6,7 +6,7 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 15:22:18 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/12/20 13:41:22 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/12/21 14:11:27 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 int	check_extension_bonus(char *path)
 {
-	size_t len;
-	int fd;
+	size_t	len;
+	int		fd;
 
 	len = ft_strlen(path);
 	if (open(path, O_DIRECTORY) >= 0)
@@ -25,13 +25,13 @@ int	check_extension_bonus(char *path)
 		close(fd);
 		return (FAILURE);
 	}
-	else 
+	else
 	{
 		fd = open(path, O_RDONLY);
 		close(fd);
-		if ((path[len - 3] != 'b' && path[len - 2] != 'e' 
-			&& path[len - 1] != 'r'
-			&& path[len - 4] != '.') || fd < 0)
+		if ((path[len - 3] != 'b' && path[len - 2] != 'e'
+				&& path[len - 1] != 'r'
+				&& path[len - 4] != '.') || fd < 0)
 			return (FAILURE);
 		else
 			return (SUCCESS);
