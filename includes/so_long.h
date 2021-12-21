73f8c92c3f518e7e81e37786a0721676fb10d845
Marine SANJUAN <marine@sanjuan.eu>
2021-12-21 16:15:55 +0100

    42 norm OK

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,51 +6,34 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 09:06:24 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/12/17 15:05:31 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/12/21 16:11:39 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <X11/keysym.h>
-#include <X11/X.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include "get_next_line.h"
-#include "../libft/libft.h"
-#include "../mlx_linux/mlx.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "get_next_line.h"
+# include "../libft/libft.h"
+# include "../mlx_linux/mlx.h"
 
 # ifndef O_DIRECTORY
-#  define O_DIRECTORY 00200000        /* must be a directory */
+#  define O_DIRECTORY 00200000
 # endif
-
-# define COIN1 "assets/coin/coin1.xpm"
-# define COIN2 "assets/coin/coin2.xpm"
-# define COIN3 "assets/coin/coin3.xpm"
-# define COIN4 "assets/coin/coin4.xpm"
-# define COIN5 "assets/coin/coin5.xpm"
-
-# define W_TOP "assets/walltop.xpm"
-# define W_BOT "assets/wallbot.xpm"
-# define W_LEFT "assets/wallleft.xpm"
-# define W_RIGHT "assets/wallright.xpm"
-# define C_TL "assets/corner3.xpm"
-# define C_TR "assets/corner4.xpm"
-# define C_BL "assets/corner2.xpm"
-# define C_BR "assets/corner1.xpm"
-
 
 # define ERROR -1
 # define FAILURE 1
 # define SUCCESS 0
-# define IMG_SIZE 60
-# define TITRE "Binding of Marine"
-// # define IMG_H 48
+# define IMG_SIZE 48
+# define TITRE "Treasure"
 # define RED "\033[0;31m"
 # define RESET "\033[0m"
 # define GREEN "\033[1;32m"
@@ -67,27 +50,51 @@
 # define D 100
 # define S 115
 # define W 119
-# define PLAYER "assets/maskdude.xpm"
-# define EXIT "assets/exit.xpm"
-# define COIN "assets/collectible.xpm"
+# define FOE "assets/foe.xpm"
+# define PLAYER "assets/player.xpm"
+# define EXIT "assets/chest_unlocked.xpm"
+# define LOCKED "assets/chest_locked.xpm"
 # define WALL "assets/wall.xpm"
 # define GROUND "assets/ground.xpm"
+# define COIN1 "assets/coin/coin1.xpm"
+# define COIN2 "assets/coin/coin2.xpm"
+# define COIN3 "assets/coin/coin3.xpm"
+# define COIN4 "assets/coin/coin4.xpm"
+# define COIN5 "assets/coin/coin5.xpm"
+# define W_TOP "assets/walls/wall_t.xpm"
+# define W_BOT "assets/walls/wall_b.xpm"
+# define W_LEFT "assets/walls/wall_l.xpm"
+# define W_RIGHT "assets/walls/wall_r.xpm"
+# define C_TL "assets/walls/corner_tl.xpm"
+# define C_TL2 "assets/walls/corner_tl2.xpm"
+# define C_TL3 "assets/walls/corner_tl3.xpm"
+# define C_TR "assets/walls/corner_tr.xpm"
+# define C_TR2 "assets/walls/corner_tr2.xpm"
+# define C_TR3 "assets/walls/corner_tr3.xpm"
+# define C_BL "assets/walls/corner_bl.xpm"
+# define C_BL2 "assets/walls/corner_bl2.xpm"
+# define C_BL3 "assets/walls/corner_bl3.xpm"
+# define C_BR "assets/walls/corner_br.xpm"
+# define C_BR2 "assets/walls/corner_br2.xpm"
+# define C_BR3 "assets/walls/corner_br3.xpm"
+# define W_RAND "assets/walls/wall_random.xpm"
 # define ERRBER "Argument is not a correct .ber file.\n"
 # define ERROTHER "At least one character of the map is not valid.\n"
 # define ERRCHARS "One of the characters \"0, 1, P, C, E\" is missing.\n"
 # define ERRPLAYER "This is a solo campaign...\n"
+# define ERRPLAYERB "There can be only one player on each side...\n"
 # define ERRREC "The map is not a rectangle.\n"
 # define ERREDGES "The edges of the map are invalid (must be walls -1-)\n"
 
 typedef struct s_map
 {
-	int 	fd;
+	int		fd;
 	int		line_count;
 	size_t	line_len;
-	int 	count_p;
-	int 	count_e;
-	int 	count_c;
-	char 	*path;
+	int		count_p;
+	int		count_e;
+	int		count_c;
+	char	*path;
 	char	**map;
 	int		can_exit;
 	int		collected;
@@ -96,103 +103,65 @@ typedef struct s_map
 
 typedef struct s_img
 {
-	void	*mlx_img; // refers to the address the func mlx_new_image returns
+	void	*mlx_img;
 	int		width;
 	int		height;
 	void	*wall;
-	void	*exit;
+	void	*exit_l;
 	void	*ground;
 	void	*player;
-	void	*coin;
-	char	*addr; // index = line_len * y + x * (bpp / 8)
-	int		bits_per_pixel; 
-	int		line_length; // amount of bytes taken by one row of our image. = image_width * (bpp / 8)
-	int		endian; // means that the bytes in computer memory are read in a certain order. Big-endian (BE) and Little-endian (LE).
+	int		line_length;
 	void	*coin1;
-	void	*coin2;
-	void	*coin3;
-	void	*coin4;
-	void	*coin5;
-	void	*coin6;
-	void	*wall_b;
-	void	*wall_t;
-	void	*wall_l;
-	void	*wall_r;
-	void	*corner_tl;
-	void	*corner_tr;
-	void	*corner_bl;
-	void	*corner_br;
+
 }	t_img;
 
-typedef struct	s_data 
+typedef struct s_data
 {
 	void	*mlx;
 	void	*win;
 	int		steps_count;
 	int		p_i;
 	int		p_j;
+	int		i;
+	int		j;
 	int		win_height;
 	int		win_width;
 	t_img	img;
 	t_map	map;
-	
-}				t_data;
 
-typedef struct s_rect
-{
-	int	x;
-	int	y;
-	int width;
-	int height;
-	int color;
-}				t_rect;
+}				t_data;
 
 /*
 **
 **  00_INIT_PARSING
 **
 */
-/* ******** create_map.c ********/
-int		retrieveLineNumber(char *path);
-void	createMap(char *path, t_data *data);
+int		retrieve_line_number(char *path);
+void	create_map(char *path, t_data *data);
+void	put_input_in_map(int row, int column, int i, t_data *data);
 void	ft_free(char **tab);
-
-/* ******** printmap.c ********/
-void	parseMap(t_data *data);
-
-/* ******** printmap.c ********/
-void	printMap(t_data *data);
-void	initMap(t_data *data);
+void	parse_map(t_data *data);
+void	init_map(t_data *data);
+void	init_player(t_data *data);
 /*
 **
 **  01_ERRORS
 **
 */
-/* ******** error_msg.c ********/
 void	error_msg(char *str, t_data *data);
-
-/* ******** check_ber.c ********/
 int		check_extension(char *path);
-
-/* ******** check_map.c ********/
-int		checkMap(t_data *data);
-int		checkChars(t_data *data);
-int		checkRectangle(t_data *data);
-int		checkEdges(int line_count, char **map);
-int 	checkTopBot(int row, char **map);
+int		check_map(t_data *data);
+int		check_chars(t_data *data);
+int		check_rectangle(t_data *data);
+int		check_edges(int line_count, char **map);
+int		check_top_bot(int row, char **map);
 /*
 **
 **  02_GRAPHICS
 **
 */
-/* ******** Pixel_utils.c ********/
-void	img_pix_put(t_img *img, int x, int y, int color);
-
-/* ******** Render.c ********/
 int		render(t_data *data);
-void	render_background(t_img *img, int color);
-int		coinSprite(t_data *data, int width, int i);
-
+void	parse_chars(t_data *data, int width, int i, int j);
 int		init_window(t_data *data);
 void	init_images(t_data *data);
 void	loop_images(t_data data);
@@ -202,17 +171,12 @@ void	destroy_images(t_data data);
 **  03_EVENTS
 **
 */
-/* ******** Handlers.c ********/
 int		handle_keypress(int keysym, t_data *data);
-int		handle_resize(t_data *data);
 int		handle_btnrealease(t_data *data);
-
 void	move_msg(t_data *data);
+void	move_player(t_data *data, char direction);
+void	collect_coins(t_data *data, char direction);
+int		win_game(t_data *data);
+int		check_next_tile(t_data *data, char direction, char tile);
 
-void	movePlayer(t_data *data, char direction);
-int		checkCollision(t_data *data, char direction);
-void	collectCoins(t_data* data, char direction);
-int		crossExit(t_data* data, char direction);
-int		winGame(t_data *data);
-int		checkNextTile(t_data *data, char direction, char tile);
 #endif
