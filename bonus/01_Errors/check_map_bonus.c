a3e32b74456c18ba6775ddc1fc1fe89d86d947e0
Marine SANJUAN <marine@sanjuan.eu>
2021-12-21 14:25:00 +0100

    42 norm ok

--- bonus/01_Errors/check_map_bonus.c
+++ bonus/01_Errors/check_map_bonus.c
@@ -6,44 +6,31 @@
 /*   By: msanjuan <msanjuan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 16:52:46 by msanjuan          #+#    #+#             */
-/*   Updated: 2021/12/21 09:00:50 by msanjuan         ###   ########.fr       */
+/*   Updated: 2021/12/21 14:24:39 by msanjuan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long_bonus.h"
 #include "../../includes/get_next_line.h"
 
-int	checkChars_bonus(t_data *data)
+int	check_chars_bonus(t_data *data)
 {
-	int i;
-	int j;
-	
-	i = 0;
-	j = 0;
-	while (data->map.map[i])
+	while (data->map.map[data->i])
 	{
-		while(data->map.map[i][j])
+		while (data->map.map[data->i][data->j])
 		{
-			if (data->map.map[i][j] == 'P')
-			{
+			if (data->map.map[data->i][data->j] == 'P')
 				data->map.count_p++;
-				data->p_i = i;
-				data->p_j = j;
-			}
-			else if (data->map.map[i][j] == 'F')
-			{
+			else if (data->map.map[data->i][data->j] == 'F')
 				data->map.count_f++;
-				data->f_i = i;
-				data->f_j = j;
-			}
-			else if (data->map.map[i][j] == 'E')
+			else if (data->map.map[data->i][data->j] == 'E')
 				data->map.count_e++;
-			else if (data->map.map[i][j] == 'C')
+			else if (data->map.map[data->i][data->j] == 'C')
 				data->map.count_c++;
-			j++;
+			data->j++;
 		}
-		j = 0;
-		i++;
+		data->j = 0;
+		data->i++;
 	}
 	if (!data->map.count_c || !data->map.count_e || !data->map.count_p)
 		return (FAILURE);
@@ -51,9 +38,9 @@ int	checkChars_bonus(t_data *data)
 		return (SUCCESS);
 }
 
-int checkTopBot_bonus(int row, char **map)
+int	check_top_bot_bonus(int row, char **map)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (map[row][i] && map[row][i] != '\n')
@@ -65,33 +52,33 @@ int checkTopBot_bonus(int row, char **map)
 	return (SUCCESS);
 }
 
-int	checkEdges_bonus(int line_count, char **map)
+int	check_edges_bonus(int line_count, char **map)
 {
-	int i;
+	int	i;
 
 	i = 1;
-	if (checkTopBot_bonus(0, map) == FAILURE 
+	if (checkTopBot_bonus(0, map) == FAILURE
 		|| checkTopBot_bonus(line_count, map) == FAILURE)
 		return (FAILURE);
-	while (i < line_count) // vérifier cotés
+	while (i < line_count)
 	{
-		if (map[i][0] != '1' || map[i][ft_strlen(map[0]) - 2] != '1') 
+		if (map[i][0] != '1' || map[i][ft_strlen(map[0]) - 2] != '1')
 			return (FAILURE);
 		i++;
 	}
 	return (SUCCESS);
 }
 
-int	checkRectangle_bonus(t_data *data)
+int	check_rectangle_bonus(t_data *data)
 {
-	int i;
-	size_t j;
+	int		i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
 	data->map.line_len = ft_strlen(data->map.map[i]) - 1;
 	while (data->map.map[i])
-	{		
+	{
 		while (data->map.map[i][j] && data->map.map[i][j] != '\n')
 			j++;
 		if (j != data->map.line_len)
@@ -102,11 +89,11 @@ int	checkRectangle_bonus(t_data *data)
 	return (SUCCESS);
 }
 
-int	checkMap_bonus(t_data *data)
+int	check_map_bonus(t_data *data)
 {
-	int i;
-	size_t j;
-	
+	int		i;
+	size_t	j;
+
 	i = 0;
 	j = 0;
 	while (data->map.map[i])
@@ -120,7 +107,7 @@ int	checkMap_bonus(t_data *data)
 		j = 0;
 		i++;
 	}
-	if (checkChars_bonus(data) == FAILURE)
+	if (check_chars_bonus(data) == FAILURE)
 		error_msg_bonus(ERRCHARS, data);
 	if (data->map.count_p > 1 || data->map.count_f > 1)
 		error_msg_bonus(ERRPLAYERB, data);
